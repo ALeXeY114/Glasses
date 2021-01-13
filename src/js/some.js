787630fda0dad6fe89e939ec16aa8912c9e6198c
alexey114 <bomkoar@gmail.com>
2021-01-13 22:45:16 +0300

    first commit

---
+++ src/js/some.js
@@ -0,0 +1,21 @@
+/*$(function () {
+
+	var nav__product = $("#navproduct"),
+		asideH = $("#aside").innerHeight(),
+	scrollOffset = 0;
+
+	$(window).on("scroll", function(){
+
+	
+		scrollOffset = $(this).scrollTop();
+
+		if(scrollOffset >= asideH) {
+			navproduct.addClass("fixed");
+		} else {
+			navproduct.removeClass("fixed");
+		}
+
+		});
+});*/
+
+
